269d06e174061be3960311ee9bc7594a02d1e503
Penny Chase <mpchase@mac.com>
2025-04-05 19:18:01 -0400

    Simple cluster reimplemented with libcluster

--- simple_cluster/lib/simple_cluster/application.ex
+++ simple_cluster/lib/simple_cluster/application.ex
@@ -8,13 +8,30 @@ defmodule SimpleCluster.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      {Cluster.Supervisor, [topologies(), [name: SimpleCluster.ClusterSupervisor]]},
       SimpleCluster.Observer,
       SimpleCluster.Ping
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: SimpleCluster.Supervisor]
+    opts = [strategy: :one_for_one, name: LibclusterCluster.Supervisor]
+
     Supervisor.start_link(children, opts)
   end
+
+  defp topologies do
+    [
+      exanmple: [
+        strategy: Cluster.Strategy.Epmd,
+        config: [
+          hosts: [
+            :"n1@127.0.0.1",
+            :"n2@127.0.0.1"
+          ]
+        ]
+      ]
+    ]
+  end
+
 end
